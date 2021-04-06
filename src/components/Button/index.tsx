cd67bfee54807acc8d3b6ad5dd082d48a149532f
Lucas Henrique <lucaa.henrique.carlos@gmail.com>
2021-04-06 10:55:43 -0300

    setup project theme first components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, MouseEvent } from 'react'
+import * as S from './styles'
+
+export type ButtonProps = {
+  children?: ReactNode
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: ReactNode
+  onClick?: () => (event: MouseEvent<HTMLButtonElement>) => void
+}
+
+const Button = ({
+  children,
+  icon,
+  size = 'medium',
+  fullWidth = false,
+  ...props
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {!!icon && icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
