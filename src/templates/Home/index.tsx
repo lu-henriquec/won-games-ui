88f2911111d43d8fcbdf0c730b887cd8d3fa6a89
Lucas Henrique <lucaa.henrique.carlos@gmail.com>
2021-04-13 15:14:22 -0300

    create home page

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,19 @@
+import { Container } from 'components/Container'
+import Footer from 'components/Footer'
+import Menu from 'components/Menu'
+
+// import * as S from './styles'
+
+const Home = () => (
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </section>
+)
+
+export default Home
